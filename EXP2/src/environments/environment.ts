bb332dc9e0928dbfe457b6bc0d2189bc3fcc9c03
Omar Del Toro <19033@portalcedi.edu.mx>
2025-03-23 22:45:37 -0600

    Authentication

--- EXP2/src/environments/environment.ts
+++ EXP2/src/environments/environment.ts
@@ -2,10 +2,22 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { initializeApp } from "firebase/app";
+
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDREeeRuuIndpA1b8KsncQvIzf8IfVrbvM",
+    authDomain: "exp2-9a9d2.firebaseapp.com",
+    projectId: "exp2-9a9d2",
+    storageBucket: "exp2-9a9d2.firebasestorage.app",
+    messagingSenderId: "403583816709",
+    appId: "1:403583816709:web:7fa8ccaf63cff367ef786c"
+  },
 };
 
+const app = initializeApp(environment.firebaseConfig);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
--- EXP2/src/main.ts
+++ EXP2/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
+
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -10,5 +15,8 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
 });
